c59dadb54c826a22c9c0953ed95e8510aabd2a77
IamPurpleBoy <purpleboy@hotmail.com>
2022-04-10 22:29:11 -0300

    Actualizacion con componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutmeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
