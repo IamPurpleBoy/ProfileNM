980726f4461aa8a5c74efa1b4e029c1a1d052c24
IamPurpleBoy <purpleboy@hotmail.com>
2022-06-03 02:57:02 -0300

    Modificado Login con Auth, Guard y ruteos del back

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { LoginComponent } from './components/login/login.component';
+import { GuardsGuard } from './servicios/guards.guard';
 
 const routes: Routes = [
-  {path: "portfolio",component:PortfolioComponent},
+  {path: "portfolio",component:PortfolioComponent, canActivate:[GuardsGuard]},
   {path: "login",component:LoginComponent},
   {path: "", redirectTo:"login" , pathMatch:"full"},
 
--- src/app/servicios/guards.guard.ts
+++ src/app/servicios/guards.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AutenticacionService } from './autenticacion.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardsGuard implements CanActivate {
+  constructor(private autenticacionService:AutenticacionService, private rutas:Router){
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    let currentUser=this.autenticacionService.UsuarioAutenticado;
+    if(currentUser && currentUser.accessToken){
+      return true;
+    } else{
+    this.rutas.navigate(['/login']);
+    return false;  
   }
-  
+  }
 }
